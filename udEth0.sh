76b980b30737c9ea2f7be6db10cc2c2fde2cf69f
temorubun <51144412+temorubun@users.noreply.github.com>
2024-10-07 05:46:14 +0900

    Update  udEth0.sh

--- udEth0.sh
+++ udEth0.sh
@@ -5,10 +5,10 @@ STATUS=$(ip link show eth1 | grep "state UP")
 
 if [ -n "$STATUS" ]; then
     echo "refresh.."
-    sudo ifconfig eth1 down
+    sudo ifconfig eth0 down
 else
     echo "refresh..."
-    sudo ifconfig eth1 up
+    sudo ifconfig eth0 up
 fi
 
 
